8e798475a4d922f64a81292d798c01adbd9a0126
Zarina-Krb <zarina.sch@yandex.ru>
2026-08-17 22:53:57 +0300

    Optimize imports

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -8,39 +8,39 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-    @Service
-    public class UserServiceImpl implements UserService {
-        @Autowired
-        private UserDao userDao;
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
 
-        @Override
-        @Transactional
-        public User add(User user) {
-            userDao.add(user);
-            return user;
-        }
+    @Override
+    @Transactional
+    public User add(User user) {
+        userDao.add(user);
+        return user;
+    }
 
-        @Override
-        @Transactional(readOnly = true)
-        public List<User> getAllUsers() {
-            return userDao.getAllUsers();
-        }
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
 
-        @Override
-        @Transactional(readOnly = true)
-        public User findById(Long id) {
-            return userDao.findById(id);
-        }
+    @Override
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userDao.findById(id);
+    }
 
-        @Override
-        @Transactional
-        public void update(User user) {
-            userDao.update(user);
-        }
+    @Override
+    @Transactional
+    public void update(User user) {
+        userDao.update(user);
+    }
 
-        @Override
-        @Transactional
-        public void delete(Long id) {
-            userDao.delete(id);
-        }
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        userDao.delete(id);
+    }
 }
